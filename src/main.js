c5c59c9c1a67d56d9990af08090c305f16b83db0
HojinChuu <cnghwls7@gmail.com>
2020-08-09 22:10:54 +0900

    init

--- src/main.js
+++ src/main.js
@@ -3,7 +3,10 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
+import Ionic from "@ionic/vue";
+import "@ionic/core/css/ionic.bundle.css";
 
+Vue.use(Ionic);
 Vue.config.productionTip = false;
 
 new Vue({
