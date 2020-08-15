f37706393c75046ba1a34fa6ce6709ca78744fc2
HojinChuu <cnghwls7@gmail.com>
2020-08-16 00:09:31 +0900

    src

--- src/components/Task/ImageSlider.vue
+++ src/components/Task/ImageSlider.vue
@@ -3,7 +3,7 @@
     <ion-gird>
       <ion-col>
         <ion-label><small>{{ image.name }}</small></ion-label>
-        <ion-img src="https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7741?d=identicon&f=y" style="width:120px; margin:auto;"></ion-img>
+        <ion-img :src="imageFile" style="width:120px; margin:auto;"></ion-img>
         <ion-button size="small">편집</ion-button>
         <ion-button color="danger" @click="remove" size="small">삭제</ion-button>
       </ion-col>
